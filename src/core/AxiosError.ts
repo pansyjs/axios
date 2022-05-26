225db8fc7129f05ddba0df86346da6f2ce09e8b5
wangxingkang <wang_xingkang@qq.com>
2022-05-26 11:52:03 +0800

    fix: 修复类型报错

--- src/core/AxiosError.ts
+++ src/core/AxiosError.ts
@@ -1,25 +1,25 @@
-import type { AxiosRequestConfig, AxiosResponse, } from '../types';
+import type { AxiosRequestConfig, AxiosResponse } from '../types';
 
 export class AxiosError<T = unknown, D = any> extends Error {
-  public code: string;
-  public config: AxiosRequestConfig<D>;
+  public code: string | undefined;
+  public config: AxiosRequestConfig<D> | undefined;
   public request: any;
   public response?: AxiosResponse<T, D>;
   public name = 'AxiosError';
   public isAxiosError = true;
 
-  static readonly ERR_FR_TOO_MANY_REDIRECTS = "ERR_FR_TOO_MANY_REDIRECTS";
-  static readonly ERR_BAD_OPTION_VALUE = "ERR_BAD_OPTION_VALUE";
-  static readonly ERR_BAD_OPTION = "ERR_BAD_OPTION";
-  static readonly ERR_NETWORK = "ERR_NETWORK";
-  static readonly ERR_DEPRECATED = "ERR_DEPRECATED";
-  static readonly ERR_BAD_RESPONSE = "ERR_BAD_RESPONSE";
-  static readonly ERR_BAD_REQUEST = "ERR_BAD_REQUEST";
-  static readonly ERR_NOT_SUPPORT = "ERR_NOT_SUPPORT";
-  static readonly ERR_INVALID_URL = "ERR_INVALID_URL";
-  static readonly ERR_CANCELED = "ERR_CANCELED";
-  static readonly ECONNABORTED = "ECONNABORTED";
-  static readonly ETIMEDOUT = "ETIMEDOUT";
+  static readonly ERR_FR_TOO_MANY_REDIRECTS = 'ERR_FR_TOO_MANY_REDIRECTS';
+  static readonly ERR_BAD_OPTION_VALUE = 'ERR_BAD_OPTION_VALUE';
+  static readonly ERR_BAD_OPTION = 'ERR_BAD_OPTION';
+  static readonly ERR_NETWORK = 'ERR_NETWORK';
+  static readonly ERR_DEPRECATED = 'ERR_DEPRECATED';
+  static readonly ERR_BAD_RESPONSE = 'ERR_BAD_RESPONSE';
+  static readonly ERR_BAD_REQUEST = 'ERR_BAD_REQUEST';
+  static readonly ERR_NOT_SUPPORT = 'ERR_NOT_SUPPORT';
+  static readonly ERR_INVALID_URL = 'ERR_INVALID_URL';
+  static readonly ERR_CANCELED = 'ERR_CANCELED';
+  static readonly ECONNABORTED = 'ECONNABORTED';
+  static readonly ETIMEDOUT = 'ETIMEDOUT';
 
   constructor(
     message: string,
@@ -35,5 +35,4 @@ export class AxiosError<T = unknown, D = any> extends Error {
     this.request = request;
     this.response = response;
   }
-
 }
